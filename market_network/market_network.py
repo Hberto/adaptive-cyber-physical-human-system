167899ce961ab13100c7c4fea7107dcb32c080c2
Herberto <herberto.werner@haw-hamburg.de>
2023-04-06 17:49:23 +0200

    market network impl

---
+++ market_network/market_network.py
@@ -0,0 +1,31 @@
+from typing import List
+import networkx as nx #ToDo: Fehler
+from agent import Consumer, Producer
+
+
+class MarketNetwork(object):
+    def __init__(self, graph: nx.Graph):
+        self._graph = graph
+        self._consumers, self._producers = self._extract_participants()
+
+    def graph(self):
+        return self._graph
+
+    def consumers(self) -> List[Consumer]:
+        return self._consumers
+
+    def producers(self) -> List[Producer]:
+        return self._producers
+
+    def _extract_participants(self):
+        consumers: List[Consumer] = []
+        producers: List[Producer] = []
+        for n in self._graph.nodes():
+            if isinstance(n, Producer):
+                producers.append(n)
+                continue
+            if isinstance(n, Consumer):
+                consumers.append(n)
+                continue
+            raise ValueError("Unsupported participant type!")
+        return consumers, producers
