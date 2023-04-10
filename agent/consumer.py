bcd58b042f7a1d750a158511171f5a37770231c4
Herberto <herberto.werner@haw-hamburg.de>
2023-04-10 09:58:00 +0200

    add storage and getter and setter for storage

--- agent/consumer.py
+++ agent/consumer.py
@@ -4,15 +4,24 @@ from agent.agent import Participant
 class Consumer(Participant):
     """Base class for all consumer agents"""
 
-    def __init__(self, demand=10.0):
+    def __init__(self, demand=0.0, storage=0.0):
         super().__init__()
         self._demand = demand
+        self._storage = storage
 
     def announce_bid(self):
-        raise NotImplementedError # ToDo: Impl
+        pass
 
     def pay(self):
         self._orders = []
+
+    # ToDo: update_storage()
+    def update_storage(self):
+        pass
+
+    @property
+    def storage(self):
+        return self._storage
 
     @property
     def demand(self):
@@ -23,3 +32,9 @@ class Consumer(Participant):
         if demand < 0:
             raise ValueError("demand cannot be negative")
         self._demand = demand
+
+    @storage.setter
+    def storage(self, storage):
+        if storage < 0:
+            raise ValueError("storage cannot be negative")
+        self._storage = storage
