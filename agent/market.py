0348a38b7e35476660af9cdb7131d35e106beb7a
Elisabeth Heinze <elisabeth_heinze@outlook.de>
2023-04-11 20:56:01 +0200

    market works

--- agent/market.py
+++ agent/market.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from shapely.geometry import LineString
 from agent.agent import Agent
 from agent.agent import Order
 from market_network.market_network import MarketNetwork
@@ -18,65 +21,63 @@ class Market(Agent):
         consumers = self._network.consumers()
         producers = self._network.producers()
 
-        amount_needed = 0
+        i_amount = 0
+        i_price = 1
+        i_agent = 2
+        i_total_amount = 3
+
         consumer_bid = []
         for c in consumers:
-            consumer_bid.append((c.bid().amount, c.bid().unit_price, c))
-            amount_needed += c.bid().amount
+            consumer_bid.append([c.bid().amount, c.bid().unit_price, c, 0])
 
-        consumer_bid.sort(key=lambda x: (x[1], x[0]))
+        consumer_bid.sort(key=lambda x: (x[i_price], x[i_amount]))
+        consumer_bid.reverse()
 
         producer_bid = []
         for p in producers:
-            producer_bid.append((p.bid().amount, p.bid().unit_price, p))
+            producer_bid.append([p.bid().amount, p.bid().unit_price, p, 0])
 
-        producer_bid.sort(key=lambda x: (x[1], x[0]))
+        producer_bid.sort(key=lambda x: (x[i_price], x[i_amount]))
 
-        amount = 0
-        price = 0
+        total_amount = 0
+        for c in consumer_bid:
+            total_amount += c[i_amount]
+            c[i_total_amount] = total_amount
 
-        for i in range(0, len(producer_bid)):
-            p_bid = producer_bid[i]
-            while p_bid[1] > consumer_bid[0][1] and amount < amount_needed:
-                amount_needed -= consumer_bid[0][0]
-                consumer_bid.pop(0)
+        total_amount = 0
+        for p in producer_bid:
+            total_amount += p[i_amount]
+            p[i_total_amount] = total_amount
 
-            if amount > amount_needed:
+        try:
+            while consumer_bid[-1][i_total_amount] > producer_bid[-1][i_total_amount] or consumer_bid[-1][i_price] < \
+                    producer_bid[-1][i_price]:
+                while consumer_bid[-1][i_total_amount] > producer_bid[-1][i_total_amount]:
+                    consumer_bid.pop()
 
-                i -= 1
-                if i >= 0:
-                    while (amount - producer_bid[i][0]) >= amount_needed:
-                        amount -= producer_bid[i][0]
-                        i -= 1
-                        if i >= 0:
-                            price = producer_bid[i][1]
-                        else:
-                            price = 0
-                            amount = 0
-                            break
-                break
-            else:
-                amount += p_bid[0]
-                price = p_bid[1]
-                if amount > amount_needed:
-                    break
+                while consumer_bid[-1][i_total_amount] < producer_bid[-1][i_total_amount] - producer_bid[-1][i_amount]:
+                    producer_bid.pop()
 
+                while consumer_bid[-1][i_price] < producer_bid[-1][i_price]:
+                    consumer_bid.pop()
+
+                while consumer_bid[-1][i_total_amount] < producer_bid[-1][i_total_amount] - producer_bid[-1][i_amount]:
+                    producer_bid.pop()
+        except IndexError:
+            consumer_bid = []
+            producer_bid = []
+
+        amount_needed = 0
         for c_bid in consumer_bid:
-            c = c_bid[2]
-            bid = c.bid()
-
-            c.add_order(Order(amount=bid.amount, unit_price=price))
+            c_bid[2].add_order(Order(amount=c_bid[i_amount], unit_price=producer_bid[-1][i_price]))
+            amount_needed += c_bid[i_amount]
 
         amount_produced = 0
         for p_bid in producer_bid:
+            if amount_produced + p_bid[i_amount] < amount_needed:
+                amount_produced += p_bid[i_amount]
+                p_bid[i_agent].add_order(Order(amount=p_bid[i_amount], unit_price=producer_bid[-1][i_price]))
 
-            if amount_produced + p_bid[0] < amount_needed:
-                amount_produced += p_bid[0]
-                p = p_bid[2]
-                bid = p.bid()
-                p.add_order(Order(amount=bid.amount, unit_price=price))
             elif amount_produced < amount_needed:
-                p = p_bid[2]
-                p.add_order(Order(amount=amount_needed - amount_produced, unit_price=price))
-                amount_produced = amount_needed
-
+                p_bid[i_agent].add_order(
+                    Order(amount=amount_needed - amount_produced, unit_price=producer_bid[-1][i_price]))
