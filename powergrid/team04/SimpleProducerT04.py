499bec6722d1f37c6834f5499b1bbba48a10a445
Herberto <herberto.werner@haw-hamburg.de>
2023-04-20 01:50:06 +0200

    SimpleProducer without adjust_Prod

--- powergrid/team04/SimpleProducerT04.py
+++ powergrid/team04/SimpleProducerT04.py
@@ -1,44 +1,68 @@
 from powergrid.base.producer import BaseProducer
 from powergrid.trade_objects import Order
 
+OPERATING_COST = 20
+START_COST = 2
+SHUTDOWN_COST = 2
+MIN_PERFORMANCE = 0
+MAX_PERFORMANCE = 100
+
 
 class Team04Producer(BaseProducer):
 
-    def __init__(self, agent_id: int):
+    def __init__(self, agent_id: int, quantity: int, price: int, current_performance: int):
         super().__init__(agent_id)
+        self._operating_cost = OPERATING_COST
+        self._start_cost = START_COST
+        self._shutdown_cost = SHUTDOWN_COST
+        self._current_performance = current_performance
+        self.quantity = quantity
+        self.price = price
 
     def get_bid(self) -> Order:
-        """
-        Returns the current order (bid) for the simulation round
-        :return: Current order of this producer
-        """
-        ...
+        return self._current_order
 
     def announce_bid(self) -> None:
+        # Notizen
+        # Speicher einbauen
+        # Auf den Market Response reagieren
+        # Balance beachten
+        # initial order beachten
+
+        # ToDo: adjust normal case and else - basic impl
         """
-        Calculates the bid of this producer in the simulation round.
-        This is the place where the individual strategies are applied.
-        :return: None
+        1. Normal Case: Sell everything, must be one higher than our costs
+        2. Storage is empty or we are broke -> can't sell anything
         """
-        ...
+        if self._balance >= 0 and self._storage > 0:
+            self.price = self._calculate_costs() + 1
+            self.quantity = self._storage + self._current_performance
+            self._current_order = Order(agent_id=self.agent_id, quantity=self.quantity, price=self.price)
+
+        if self._balance < 0 or self._storage == 0:
+            self.price = 0
+            self.quantity = 0
+            self._current_order = Order(agent_id=self.agent_id, quantity=self.quantity, price=self.price)
+
+        else:
+            self.quantity = self._storage + self._current_performance
+            self.price = 1  # low price, sell anyway -> CHANGE
+            self._current_order = Order(agent_id=self.agent_id, quantity=self.quantity, price=self.price)
 
     def process_orders(self) -> None:
-        """
-        Processes the accepted orders from the consumers.
-        :return:
-        """
-        ...
+        new_storage = self._storage + self._current_performance - self._market_response.actual_quantity
+        if new_storage < 0:
+            self._storage = 0
+        self._storage = new_storage
 
     def compute_cost_and_update_profit(self) -> None:
-        """
-        Compute costs and update profit margin
-        :return:
-        """
-        ...
+        self._balance += self._calculate_total_amount_my_sell() - self._calculate_costs()
 
     def adjust_production(self) -> None:
-        """
-        Adjust production based on a strategy and market_response
-        :return:
-        """
-        ...
+        pass
+
+    def _calculate_costs(self) -> int:
+        return self._start_cost + self._shutdown_cost + self._operating_cost + self._market_response.fee
+
+    def _calculate_total_amount_my_sell(self) -> int:
+        return self._market_response.actual_quantity * self._market_response.market_unit_price
