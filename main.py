3ac6395098fd22b8fa408aa018fd58ad8e5ce84e
Herberto <herberto.werner@haw-hamburg.de>
2023-04-19 17:53:09 +0200

    constant added for rounds

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from simulation import Simulation
 from powergrid.team03.consumer import Team03Consumer
 from powergrid.team03.producer import Team03Producer
 
+ROUNDS = 5
 
 if __name__ == "__main__":
-    consumers = [Team03Consumer(i) for i in range(5)]
-    producers = [Team03Producer(i + 5) for i in range(5)]
+    consumers = [Team03Consumer(i) for i in range(ROUNDS)]
+    producers = [Team03Producer(i + 5) for i in range(ROUNDS)]
     market = Market(consumers, producers)
-    Simulation.run(5, consumers, producers, market)
+    Simulation.run(ROUNDS, consumers, producers, market)
