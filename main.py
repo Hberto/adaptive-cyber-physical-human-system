458764c3152e3672d3153546f1b0284843581cbc
Herberto <herberto.werner@haw-hamburg.de>
2023-04-24 13:39:43 +0200

    new main with env variables

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import os
+
 from market import Market
 from powergrid.team03.consumer import Team03Consumer
 from powergrid.team03.producer import Team03Producer
 from simulation import Simulation
+from dotenv import load_dotenv
 
-ROUNDS = 5
 
 if __name__ == "__main__":
-    consumers = [Team03Consumer(i) for i in range(ROUNDS)]
-    producers = [Team03Producer(i + 5) for i in range(ROUNDS)]
+    load_dotenv()
+
+    num_agents = int(os.getenv('NUM_AGENTS'))
+    num_producers = int(float(os.getenv('PRODUCER_PROB')) * num_agents)
+    num_consumers = num_agents - num_producers
+
+    consumers = [Team03Consumer(i) for i in range(num_consumers)]
+    producers = [Team03Producer(i + 5) for i in range(num_producers)]
     market = Market(consumers, producers)
-    Simulation.run(ROUNDS, consumers, producers, market)
+    Simulation.run(int(os.getenv('NUM_ROUNDS')), consumers, producers, market)
